96b69d2b23c88ea908b2fecc92563dc7aefeb7f9
linkfrg <link071209@gmail.com>
2024-09-21 01:56:48 +0500

    feat(control_center): use Widget.RevealerWindow

--- .config/ignis/modules/control_center/__init__.py
+++ .config/ignis/modules/control_center/__init__.py
@@ -1,4 +1,3 @@
-from typing import Any
 from ignis.widgets import Widget
 from ignis.app import IgnisApp
 from .volume import volume_control
@@ -6,10 +5,9 @@ from .quick_settings import quick_settings
 from .user import user
 from .media import media
 from .notification_center import notification_center
-from ignis.utils import Utils
-from gi.repository import GObject
 
 app = IgnisApp.get_default()
+
 
 def control_center_widget() -> Widget.Box:
     return Widget.Box(
@@ -26,47 +24,32 @@ def control_center_widget() -> Widget.Box:
     )
 
 
-class control_center(Widget.Window):
-    def __init__(self) -> None:
-        self._revealer = Widget.Revealer(transition_type="slide_left", child=control_center_widget(), transition_duration=300, reveal_child=True)
-        self._box = Widget.Box(
-            child=[
-                Widget.Button(
-                    vexpand=True,
-                    hexpand=True,
-                    css_classes=["unset"],
-                    on_click=lambda x: app.close_window("ignis_CONTROL_CENTER"),
-                ),
-                self._revealer,
-            ],
-        )
-        super().__init__(
-            visible=False,
-            popup=True,
-            kb_mode="on_demand",
-            layer="top",
-            css_classes=["unset"],
-            anchor=["top", "right", "bottom", "left"],
-            namespace="ignis_CONTROL_CENTER",
-            child=self._box,
-        )
-
-    def set_property(self, prop_name: str, value: Any) -> None:
-        if prop_name == "visible":
-            if value:
-                super().set_property(prop_name, value)
-            else:
-                Utils.Timeout(ms=self._revealer.transition_duration, target=lambda asd=super(): asd.set_property(prop_name, value))
-            self._revealer.reveal_child = value
-            self.notify("visible")
-        else:
-            super().set_property(prop_name, value)
-
-    @GObject.Property
-    def visible(self) -> bool:
-        return self._revealer.reveal_child
-
-    @visible.setter
-    def visible(self, value: bool) -> None:
-        super().set_visible(value)
-        # self._revealer.props.reveal_child = value
+def control_center() -> Widget.RevealerWindow:
+    revealer = Widget.Revealer(
+        transition_type="slide_left",
+        child=control_center_widget(),
+        transition_duration=300,
+        reveal_child=True,
+    )
+    box = Widget.Box(
+        child=[
+            Widget.Button(
+                vexpand=True,
+                hexpand=True,
+                css_classes=["unset"],
+                on_click=lambda x: app.close_window("ignis_CONTROL_CENTER"),
+            ),
+            revealer,
+        ],
+    )
+    return Widget.RevealerWindow(
+        visible=False,
+        popup=True,
+        kb_mode="on_demand",
+        layer="top",
+        css_classes=["unset"],
+        anchor=["top", "right", "bottom", "left"],
+        namespace="ignis_CONTROL_CENTER",
+        child=box,
+        revealer=revealer,
+    )
