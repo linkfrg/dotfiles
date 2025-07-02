cf5931d774bef63b1f2b380da74b893ddac769d9
Link <linkfrg.dev@proton.me>
2025-07-03 01:23:06 +0500

    chore: hot-inject SCSS colors into `style.scss`
    ...using custom compiler function

--- ignis/config.py
+++ ignis/config.py
@@ -1,3 +1,4 @@
+import os
 from ignis import utils
 from ignis.app import IgnisApp
 from ignis.services.wallpaper import WallpaperService
@@ -11,18 +12,44 @@ from modules import (
     Settings,
 )
 from ignis.css_manager import CssManager, CssInfoPath
+from user_options import user_options
 
 app = IgnisApp.get_default()
 css_manager = CssManager.get_default()
 WallpaperService.get_default()
+
+
+def format_scss_var(name: str, val: str) -> str:
+    return f"${name}: {val};\n"
+
+
+def patch_style_scss(path: str) -> str:
+    with open(path) as file:
+        contents = file.read()
+
+    scss_colors = ""
+
+    for key, value in user_options.material.colors.items():
+        scss_colors += format_scss_var(key, value)
+
+    string = (
+        format_scss_var("darkmode", str(user_options.material.dark_mode).lower())
+        + scss_colors
+        + contents
+    )
+
+    return utils.sass_compile(
+        string=string, extra_args=["--load-path", utils.get_current_dir()]
+    )
+
 
 app.add_icons(f"{utils.get_current_dir()}/icons")
 
 css_manager.apply_css(
     CssInfoPath(
         name="main",
-        path=utils.get_current_dir() + "/style.scss",
-        compiler_function=lambda path: utils.sass_compile(path=path),
+        path=os.path.join(utils.get_current_dir(), "style.scss"),
+        compiler_function=patch_style_scss,
     )
 )
 
