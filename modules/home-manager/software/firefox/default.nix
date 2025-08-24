fe8ea853890227e2a64700851036e975d0d20a0b
Link <linkfrg.dev@proton.me>
2025-08-25 00:55:05 +0500

    make firefox default browser

--- modules/home-manager/software/firefox/default.nix
+++ modules/home-manager/software/firefox/default.nix
@@ -16,6 +16,18 @@ in {
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "default-web-browser" = ["firefox.desktop"];
+            "text/html" = ["firefox.desktop"];
+            "x-scheme-handler/http" = ["firefox.desktop"];
+            "x-scheme-handler/https" = ["firefox.desktop"];
+            "x-scheme-handler/about" = ["firefox.desktop"];
+            "x-scheme-handler/unknown" = ["firefox.desktop"];
+          };
+        };
+
         programs.firefox = {
           enable = true;
 
