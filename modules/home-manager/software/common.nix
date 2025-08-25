4305003ea4f48857368858236e58e75447c6f793
Link <linkfrg.dev@proton.me>
2025-08-25 20:21:16 +0500

    set eog default for images and mpv for videos

--- modules/home-manager/software/common.nix
+++ modules/home-manager/software/common.nix
@@ -23,5 +23,25 @@ in {
       xorg.xeyes
       uxplay
     ];
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # Images
+        "image/jpeg" = ["eog.desktop"];
+        "image/png" = ["eog.desktop"];
+        "image/gif" = ["eog.desktop"];
+        "image/webp" = ["eog.desktop"];
+        "image/tiff" = ["eog.desktop"];
+        "image/bmp" = ["eog.desktop"];
+
+        # Videos
+        "video/mp4" = ["mpv.desktop"];
+        "video/x-matroska" = ["mpv.desktop"]; # mkv
+        "video/webm" = ["mpv.desktop"];
+        "video/x-msvideo" = ["mpv.desktop"]; # avi
+        "video/quicktime" = ["mpv.desktop"]; # mov
+      };
+    };
   };
 }
