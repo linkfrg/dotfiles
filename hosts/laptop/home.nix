f151d6bc324e1ac3d87407698804d53d65fe4833
Link <linkfrg.dev@proton.me>
2025-09-12 20:48:29 +0500

    hyprland(laptop): set scaling to 1.2

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -22,4 +22,14 @@
       };
     };
   };
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1, 1920x1080@60, 0x0, 1.2"
+    ];
+
+    xwayland = {
+      force_zero_scaling = true;
+    };
+  };
 }
