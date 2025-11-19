402c883ceb032479f5466f7dd521df4db0b03f9a
Link <linkfrg.dev@proton.me>
2025-11-20 00:17:16 +0500

    niri: change keybinds

--- home/desktop/niri/binds.nix
+++ home/desktop/niri/binds.nix
@@ -35,28 +35,22 @@
     "Mod+S".action = focus-window-or-workspace-down;
     "Mod+D".action = focus-column-right;
 
-    "Mod+Shift+W".action = move-column-to-workspace-up;
-    "Mod+Shift+A".action = move-column-left;
-    "Mod+Shift+S".action = move-column-to-workspace-down;
-    "Mod+Shift+D".action = move-column-right;
+    "Mod+Ctrl+W".action = move-column-to-workspace-up;
+    "Mod+Ctrl+A".action = move-column-left;
+    "Mod+Ctrl+S".action = move-column-to-workspace-down;
+    "Mod+Ctrl+D".action = move-column-right;
+
+    "Mod+Alt+W".action = focus-monitor-up;
+    "Mod+Alt+A".action = focus-monitor-left;
+    "Mod+Alt+S".action = focus-monitor-down;
+    "Mod+Alt+D".action = focus-monitor-right;
+
+    "Mod+Alt+Ctrl+W".action = move-column-to-monitor-up;
+    "Mod+Alt+Ctrl+A".action = move-column-to-monitor-left;
+    "Mod+Alt+Ctrl+S".action = move-column-to-monitor-down;
+    "Mod+Alt+Ctrl+D".action = move-column-to-monitor-right;
 
     "Mod+grave".action = toggle-overview;
-
-    "Mod+Home".action = focus-column-first;
-    "Mod+End".action = focus-column-last;
-    "Mod+Shift+Home".action = move-column-to-first;
-    "Mod+Shift+End".action = move-column-to-last;
-
-    "Mod+Ctrl+W".action = focus-monitor-up;
-    "Mod+Ctrl+A".action = focus-monitor-left;
-    "Mod+Ctrl+S".action = focus-monitor-down;
-    "Mod+Ctrl+D".action = focus-monitor-right;
-
-    "Mod+Shift+Ctrl+W".action = move-column-to-monitor-up;
-    "Mod+Shift+Ctrl+A".action = move-column-to-monitor-left;
-    "Mod+Shift+Ctrl+S".action = move-column-to-monitor-down;
-    "Mod+Shift+Ctrl+D".action = move-column-to-monitor-right;
-
     "Mod+G".action = consume-or-expel-window-left;
     "Mod+H".action = consume-or-expel-window-right;
 
@@ -78,7 +72,7 @@
     "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
 
     "Print".action = spawn "grimshot" "--notify" "savecopy" "output";
-    "Mod+Shift+X".action = spawn "grimshot" "--notify" "savecopy" "area";
+    "Mod+Shift+S".action = spawn "grimshot" "--notify" "savecopy" "area";
 
     "Mod+Escape" = {
       allow-inhibiting = false;
