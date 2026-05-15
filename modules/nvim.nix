3f739b8d0c29f409d4ba9f589c7950049863bcd0
Link <linkfrg.dev@proton.me>
2026-05-15 10:30:06 +0500

    refactor: move neovim wrapper to flake-parts modules

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,9 +1,42 @@
-{
-  flake.homeModules.nvim = {pkgs, ...}: {
-    home.packages = [
-      (import ../config/nvim {inherit pkgs;})
-    ];
+{self, ...}: {
+  perSystem =
+    { pkgs, lib, ... }:
+    let
+      bins = with pkgs; [
+        ripgrep
+        fd
+        fzf
+        gcc
+        unzip
+        luajitPackages.luarocks
+        pyright
+        gnumake
+        stylua
+        lua-language-server
+        tree-sitter
+        rust-analyzer
+        ruff
+        nil
+      ];
+    in
+    {
+      packages.mynvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+        withPython3 = true;
+        wrapRc = false;
+        wrapperArgs = lib.strings.concatStringsSep " " [
+          ''--suffix PATH : "${lib.makeBinPath bins}"''
+        ];
+      };
+    };
 
-    home.sessionVariables."EDITOR" = "nvim";
-  };
+  flake.homeModules.nvim =
+    { pkgs, ... }:
+    {
+      home.packages = [
+        self.packages.${pkgs.system}.mynvim
+      ];
+
+      home.sessionVariables."EDITOR" = "nvim";
+    };
+
 }
